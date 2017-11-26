a566510cee5b0733d9ad4ff6554751a17518588b
Ali <ali_abbas89@hotmail.com>
2017-11-26 18:08:10 +1100

    Accessing web api code from the page

--- CarsalesMvcApp/CarsalesMvcApp/Controllers/VehicleController.cs
+++ CarsalesMvcApp/CarsalesMvcApp/Controllers/VehicleController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CarsalesDataAccess;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CarsalesMvcApp.Controllers
 {
@@ -11,11 +14,43 @@ namespace CarsalesMvcApp.Controllers
     {
         CarsalesDBEntities entities = new CarsalesDBEntities();
         // GET: Vehicle
-        public ActionResult Index()
-        {
-            return View();
-        }
 
+        string BaseUrl = "http://localhost:56052";
+        public async Task<ActionResult> Index()
+        {
+            List<vehicle> objVeh = new List<vehicle>();
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+
+                client.DefaultRequestHeaders.Clear();
+
+                //define request data format
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Sending request to find web api Rest service resouces findAll
+                HttpResponseMessage Res = await client.GetAsync("api/vehicle");
+
+                //Checking the response is successfull or not which is sent using httpClinet
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the respoinse details recieved from web api
+                    var VehicleResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response from web api and storing into Vehicle list
+
+                    objVeh = JsonConvert.DeserializeObject<List<vehicle>>(VehicleResponse);
+
+
+                }
+
+            }
+
+            return View(objVeh);
+        }
+          
         public JsonResult GetVehicles()
         {
             var result = entities.vehicles.ToList();
--- CarsalesMvcApp/CarsalesWebApi/Controllers/VehicleController.cs
+++ CarsalesMvcApp/CarsalesWebApi/Controllers/VehicleController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 using CarsalesDataAccess;
 using Newtonsoft.Json;
 using System.Net.Http;
@@ -11,7 +10,7 @@ using System.Net.Http;
 namespace CarsalesWebApi.Controllers
 {
 
-    [System.Web.Http.RoutePrefix("api/vehicles")]
+    [System.Web.Http.RoutePrefix("api/vehicle")]
     public class VehicleController : ApiController
     {
         private CarsalesDataAccess.CarsalesDBEntities objDe = new CarsalesDBEntities();
@@ -25,6 +24,40 @@ namespace CarsalesWebApi.Controllers
             response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
             return response;
         }
-       
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("{id}")]
+        public HttpResponseMessage find(int id)
+        {
+            var serializeData = JsonConvert.SerializeObject(objDe.vehicles.Find(id));
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            response.Content = new StringContent(serializeData);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            return response;
+        }
+
+        [System.Web.Http.HttpPost]
+        public void create(vehicle objVeh)
+        {
+            objDe.vehicles.Add(objVeh);
+            objDe.SaveChanges();
+        }
+
+
+        [System.Web.Http.HttpPut]
+        public void update(vehicle objVeh)
+        {
+            objDe.Entry<vehicle>(objVeh).State = System.Data.Entity.EntityState.Modified;
+            objDe.SaveChanges();
+        }
+
+        [System.Web.Http.HttpDelete]
+        [System.Web.Http.Route("{id}")]
+        public void delete(int id)
+        {
+            objDe.vehicles.Remove(objDe.vehicles.Find(id));
+            objDe.SaveChanges();
+        }
+        
     }
 }
